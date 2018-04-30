e56b72d267d30d99a08bfd5b097efefcaa59ab88
Sirtago <Niall.k.sharma@gmail.com>
2018-04-30 16:59:10 -0500

    Commit

---
+++ Stealth Point And Click/Assets/Scripts/Collisions.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+
+public class Collisions : MonoBehaviour {
+    public Movement movement;
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+        }
+    }
+}
---
+++ Stealth Point And Click/Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    public float speed = 10f;
+    public float right = 10f;
+    public float left = 10f;
+    public Rigidbody rb;
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        rb.AddForce(0, 1, speed * Time.deltaTime);
+    if ( Input.GetKey("d"))
+        {
+            rb.AddForce(right, 0, 0 * Time.deltaTime);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-right, 0, 0 * Time.deltaTime);
+        }
+    }
+
+}
