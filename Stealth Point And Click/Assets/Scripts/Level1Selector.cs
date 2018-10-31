1102cf4c78281435e015b784b38b92fceb432dfa
Sirtago <Niall.k.sharma@gmail.com>
2018-10-31 16:36:07 -0500

    123

---
+++ Stealth Point And Click/Assets/Scripts/Level1Selector.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Level1Selector : MonoBehaviour {
+    public void Selectlevel1(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
